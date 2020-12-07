61efb120dd6a0fb37ec14d2f461b107c904a062a
moonsang <anstkd07@gmail.com>
2020-12-07 14:48:12 +0900

    add JSDoc

--- step-1.js
+++ step-1.js
@@ -1,5 +1,12 @@
 const readline = require('readline');
 
+/**
+ * @description pushWord 메서드는 문제 조건대로 문자열을 밀어준다.
+ * @param {string} word 밀어야 하는 단어
+ * @param {number} number 밀어야 하는 횟수
+ * @param {string} direction 밀어야 하는 방향
+ * @returns {string} result 밀린 문자열
+ */
 function pushWord({word, number, direction}) {
   let result = word;
   for (let i = 0 ; i < number ; i++) {
@@ -13,6 +20,10 @@ function pushWord({word, number, direction}) {
   return result;
 }
 
+/**
+ * @description validateRawData 메서드는 입력받은 데이터가 문제 조건에 맞는지 검사한다.
+ * @param {Array} rawData 입력받은 Input 값
+ */
 function validateRawData(rawData) {
   if (rawData.length !== 3) {
     throw new Error("문제 조건에 맞는 3개 파라미터를 입력하셔야 합니다.");
@@ -27,6 +38,11 @@ function validateRawData(rawData) {
   }
 }
 
+/**
+ * @description preprocessRawData 메서드는 입력받은 횟수 수가 음수일 경우 방향을 바꿔주고, 입력받은 데이터를 객체로 변환시켜 준다.
+ * @param {Array} rawData 입력받은 Input 값
+ * @returns {Object} data 각 Input값의 의미를 Key로 갖도록 변환된 객체
+ */
 function preprocessRawData(rawData) {
   const data = {};
 
