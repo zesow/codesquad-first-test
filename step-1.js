d5b621bac1723e283717908e70d31635608bf8df
moonsang <anstkd07@gmail.com>
2020-12-07 13:54:48 +0900

    add input validate cases

--- step-1.js
+++ step-1.js
@@ -1,5 +1,3 @@
-const readline = require('readline');
-
 function pushWord({word, number, direction}) {
   return "";
 }
@@ -8,27 +6,37 @@ function validateRawData(rawData) {
   if (rawData.length !== 3) {
     throw new Error("문제 조건에 맞는 3개 파라미터를 입력하셔야 합니다.");
   }
+
+  if (isNaN(parseInt(rawData[1]))) {
+    throw new Error("두 번째 파라미터는 반드시 숫자여야 합니다.");
+  }
+
+  if (!(rawData[2].toLowerCase() === 'l' || rawData[2].toLowerCase() === 'r')) {
+    throw new Error("세 번째 파라미터는 반드시 left나 right 중 하나여야 합니다.");
+  }
+
 }
 
 function main() {
+  const readline = require('readline');
   const data = {};
-  const readLine = readline.createInterface({
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  readLine.on("line", function(line) {
-    readLine.close();
+  rl.on("line", function(line) {
     const rawData = line.split(" ");
     validateRawData(rawData);
-
+    
     data.word = rawData[0];
     data.number = parseInt(rawData[1]);
     data.direction = rawData[2];
     
     const result = pushWord(data);
     console.log(result);
-
+    
+    rl.close();
 
   }).on("close", function() {
     process.exit();
